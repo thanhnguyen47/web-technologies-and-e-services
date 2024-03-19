2cae877ea70e574ccf747218d819bafb221a32f9
thanhnguyen <thanhcm473@gmail.com>
2024-03-19 19:45:30 +0700

    submmit homework week 5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import axios from 'axios';
+import { useEffect, useMemo,useState } from 'react';
+function Element({data}){
+  const {id,name,phone,username,website,email,address,company} = data;
+  console.log(data);
+  return(
+    <div className='Element'>
+      <div><span>Id:</span> {id}</div>
+      <div><span>Name:</span> {name}</div>
+      <div><span>Phone number:</span> {phone}</div>
+      <div><span>Username:</span> {username}</div>
+      <div><span>Website:</span> {website}</div>
+      <div><span>Email:</span> {email}</div>
+      <div><span>Address: </span>{`${address.city} ${address.street} ${address.suite} $${address.zipcode}`}</div>
+      <div><span>Company: </span>{`${company.name} ${company.bs} ${company.catchPhrase}`}</div>
     </div>
-  );
-}
 
-export default App;
+  )
+}
+export default function App(){
+  const [dataset,setDataset]=useState();
+  
+  useEffect(()=>{
+    const fetch = async()=>{
+      const data = await axios.get('https://jsonplaceholder.typicode.com/users');
+      console.log(data.data||"");
+      setDataset(data.data);
+    }
+    fetch();
+  },[]);
+  const [id,setid]=useState(0);
+  const next = ()=>{
+    if(id<dataset.length-1)setid(prev=>prev+1);
+  }
+  const back = ()=>{
+    if(id>0)setid(prev=>prev-1);
+  }
+
+  return (
+    <div className='container'>
+
+    {dataset?<Element data = {dataset[id]}/>:""}
+      <button onClick={back} style = {{marginLeft:"60%"}}>Back</button>
+      <button onClick={next}>Next</button>
+    </div>
+
+  )
+}
